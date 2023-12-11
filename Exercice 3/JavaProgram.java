50f6ff7b9eef230921656d9a2794c256d32ce048
Lucas Hoareau <lucas.hoareau@epitech.eu>
2023-12-11 11:56:28 +0400

    Exercice 4

---
+++ Exercice 3/JavaProgram.java
@@ -0,0 +1,36 @@
+public class JavaProgram {
+    public static void main(String []args) {
+        vehicle voiture1 = new vehicle("Volkswagen", "Scirocco", 2008);
+        car voiture2 = new car("Ferrari", "Testarossa", 1984, 1656, 4);
+        truck voiture3 = new truck("Scania", "Frostfire", 2022, 6000, 6);
+        motorcycle voiture4 = new motorcycle("Kawazaki", "Ninja", 2013, 170, 2);
+        
+        System.out.println("--------------------------------");
+        System.out.println("Exercice 1 Output\n");
+
+        voiture1.Describe();
+        voiture1.Accelerate();
+
+
+        System.out.println("\n--------------------------------");
+        System.out.println("Exercice 2 Output\n");
+
+        voiture2.Describe();
+        System.out.println();
+        voiture3.Describe();
+        System.out.println();
+        voiture4.Describe();
+        System.out.println();
+
+        System.out.println("--------------------------------");
+
+        System.out.println();
+        voiture2.Accelerate();
+
+        System.out.println();
+        voiture3.Accelerate();
+
+        System.out.println();
+        voiture4.Accelerate();
+    }
+}
---
+++ Exercice 3/car.java
@@ -0,0 +1,20 @@
+public class car extends vehicle {
+    int Weigh;
+    int Wheels;
+
+    public car(String brand, String model, int year, int weigh, int wheels) {
+        super(brand, model, year);
+        Weigh = weigh;
+        Wheels = wheels;
+    }
+
+    public void Describe()  {
+        System.out.print("I'm a " + Brand + " " + Model);
+        System.out.println(" made in " + Year + ".");
+        System.out.print("I am a car, I weight ");
+        System.out.println(Weigh + " and I have " + Wheels + " Wheels.");
+    }
+    public void Accelerate()    {
+        System.out.println("My " + Brand + " goes like this : VROOOooooooom !");
+    }
+}
